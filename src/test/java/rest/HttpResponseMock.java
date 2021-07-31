3a3e95297d49984b228bb5a553bf463f3cc9c3ee
Frank Giordano <giofr01@yahoo.com>
2021-07-31 16:19:10 -0400

    JsonRequest Unit Tests
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

---
+++ src/test/java/rest/HttpResponseMock.java
@@ -0,0 +1,221 @@
+package rest;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.*;
+import org.apache.http.params.HttpParams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Locale;
+
+public class HttpResponseMock {
+
+    private HttpResponse httpResponse =  new HttpResponse() {
+
+        private String json = "{\"data\":{}}";
+
+        @Override
+        public StatusLine getStatusLine() {
+            StatusLine sl = new StatusLine() {
+                @Override
+                public ProtocolVersion getProtocolVersion() {
+                    return null;
+                }
+
+                @Override
+                public int getStatusCode() {
+                    return 200;
+                }
+
+                @Override
+                public String getReasonPhrase() {
+                    return json;
+                }
+            };
+            return sl;
+        }
+
+        @Override
+        public void setStatusLine(StatusLine statusLine) {
+
+        }
+
+        @Override
+        public void setStatusLine(ProtocolVersion protocolVersion, int i) {
+
+        }
+
+        @Override
+        public void setStatusLine(ProtocolVersion protocolVersion, int i, String s) {
+
+        }
+
+        @Override
+        public void setStatusCode(int i) throws IllegalStateException {
+
+        }
+
+        @Override
+        public void setReasonPhrase(String s) throws IllegalStateException {
+
+        }
+
+        @Override
+        public HttpEntity getEntity() {
+            return new HttpEntity() {
+                @Override
+                public boolean isRepeatable() {
+                    return false;
+                }
+
+                @Override
+                public boolean isChunked() {
+                    return false;
+                }
+
+                @Override
+                public long getContentLength() {
+                    return 0;
+                }
+
+                @Override
+                public Header getContentType() {
+                    return null;
+                }
+
+                @Override
+                public Header getContentEncoding() {
+                    return null;
+                }
+
+                @Override
+                public InputStream getContent() throws IOException, UnsupportedOperationException {
+                    InputStream stubInputStream =
+                            IOUtils.toInputStream(json, "UTF-8");
+                    return stubInputStream;
+                }
+
+                @Override
+                public void writeTo(OutputStream outputStream) throws IOException {
+
+                }
+
+                @Override
+                public boolean isStreaming() {
+                    return false;
+                }
+
+                @Override
+                public void consumeContent() throws IOException {
+
+                }
+            };
+        }
+
+        @Override
+        public void setEntity(HttpEntity httpEntity) {
+
+        }
+
+        @Override
+        public Locale getLocale() {
+            return null;
+        }
+
+        @Override
+        public void setLocale(Locale locale) {
+
+        }
+
+        @Override
+        public ProtocolVersion getProtocolVersion() {
+            return null;
+        }
+
+        @Override
+        public boolean containsHeader(String s) {
+            return false;
+        }
+
+        @Override
+        public Header[] getHeaders(String s) {
+            return new Header[0];
+        }
+
+        @Override
+        public Header getFirstHeader(String s) {
+            return null;
+        }
+
+        @Override
+        public Header getLastHeader(String s) {
+            return null;
+        }
+
+        @Override
+        public Header[] getAllHeaders() {
+            return new Header[0];
+        }
+
+        @Override
+        public void addHeader(Header header) {
+
+        }
+
+        @Override
+        public void addHeader(String s, String s1) {
+
+        }
+
+        @Override
+        public void setHeader(Header header) {
+
+        }
+
+        @Override
+        public void setHeader(String s, String s1) {
+
+        }
+
+        @Override
+        public void setHeaders(Header[] headers) {
+
+        }
+
+        @Override
+        public void removeHeader(Header header) {
+
+        }
+
+        @Override
+        public void removeHeaders(String s) {
+
+        }
+
+        @Override
+        public HeaderIterator headerIterator() {
+            return null;
+        }
+
+        @Override
+        public HeaderIterator headerIterator(String s) {
+            return null;
+        }
+
+        @Override
+        public HttpParams getParams() {
+            return null;
+        }
+
+        @Override
+        public void setParams(HttpParams httpParams) {
+
+        }
+    };
+
+    public HttpResponse getHttpResponse() {
+        return httpResponse;
+    }
+
+}
