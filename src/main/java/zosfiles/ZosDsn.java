2f5354d7adc1d162eff82664c63a5726e9a5c0f4
Leonid Baranov <leonid.baranov@broadcom.com>
2021-08-12 07:35:31 +0200

    Delete datasets and members
    Signed-off-by: Leonid Baranov <leonid.baranov@broadcom.com>

--- src/main/java/zosfiles/ZosDsn.java
+++ src/main/java/zosfiles/ZosDsn.java
@@ -11,6 +11,13 @@ public class ZosDsn {
 
     private static final Logger LOG = LogManager.getLogger(ZosDsn.class);
 
+    /**
+     * Replaces a content of a dataset or a dataset member with a new content
+     * The new dataset member will be created if a dataset member is not exists
+     * @param connection is a connection object
+     * @param dataSetName is the name of a dataset or a dataset member (f.e. DATASET.LIB(MEMBER))
+     * @param content is a new content of the dataset or a dataset member
+     */
     public static void writeDsn(ZOSConnection connection, String dataSetName, String content) {
         Util.checkNullParameter(dataSetName == null, "dataSetName is null");
         Util.checkStateParameter(dataSetName.isEmpty(), "dataSetName is empty");
@@ -23,6 +30,31 @@ public class ZosDsn {
             LOG.info(url);
 
             ZoweRequest request = new TextPutRequest(connection, url, content);
+            Response response = request.executeHttpRequest();
+            UtilDataset.checkHttpErrors(response, dataSetName);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Delete dataset or a dataset member
+     * @param connection is a connection object
+     * @param dataSetName is the name of a dataset or a dataset member (f.e. 'DATASET.LIB(MEMBER)')
+     */
+    public static void deleteDsn(ZOSConnection connection, String dataSetName) {
+        Util.checkNullParameter(dataSetName == null, "dataSetName is null");
+        Util.checkStateParameter(dataSetName.isEmpty(), "dataSetName is empty");
+        Util.checkConnection(connection);
+
+        String url = "https://" + connection.getHost() + ":" + connection.getPort()
+                + ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_DS_FILES + "/" + dataSetName;
+
+        try {
+            LOG.info(url);
+
+            ZoweRequest request = new JsonDeleteRequest(connection, url);
             Response response = request.executeHttpRequest();
             UtilDataset.checkHttpErrors(response, dataSetName);
 
