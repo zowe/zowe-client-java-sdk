b6f495270e9f4503ca56bee935fad286293b050c
Leonid Baranov <leonid.baranov@broadcom.com>
2021-08-13 10:10:19 +0200

    Fix post request
    Signed-off-by: Leonid Baranov <leonid.baranov@broadcom.com>

--- src/main/java/rest/JsonPostRequest.java
+++ src/main/java/rest/JsonPostRequest.java
@@ -72,7 +72,11 @@ public class JsonPostRequest extends ZoweRequest {
 
             JSONParser parser = new JSONParser();
             try {
-                return new Response(Optional.ofNullable(parser.parse(result)), Optional.ofNullable(statusCode));
+                if (result.isEmpty()) {
+                    return new Response(Optional.empty(), Optional.ofNullable(statusCode));
+                } else {
+                    return new Response(Optional.ofNullable(parser.parse(result)), Optional.ofNullable(statusCode));
+                }
             } catch (ParseException e) {
                 e.printStackTrace();
             }
