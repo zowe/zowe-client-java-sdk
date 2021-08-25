c48ceacae879c85854764f217466ad185b6d1287
Leonid Baranov <nurkambay@gmail.com>
2021-08-25 08:40:03 +0200

    Fix NPE any typos
    Signed-off-by: Leonid Baranov <nurkambay@gmail.com>

--- src/main/java/zosjobs/DeleteJobs.java
+++ src/main/java/zosjobs/DeleteJobs.java
@@ -49,23 +49,23 @@ public class DeleteJobs {
      * @throws Exception error on submitting
      * @author Nikunj goyal
      */
-    public Response deletejob() throws Exception {
-        return this.deleteJobCommon(null);
+    public Response deleteJob() throws Exception {
+        return this.deleteJobCommon(new DeleteJobParams(new DeleteJobParams.Builder()));
     }
 
     /**
      * Delete a job that resides in a z/OS data set.
      *
-     * @param parms delete job parameters, see DeleteJobParams object
+     * @param params delete job parameters, see DeleteJobParams object
      * @return job document with details about the submitted job
      * @throws Exception error on submitting
      * @author Nikunj goyal
      */
-    public Response deleteJobCommon(DeleteJobParams parms) throws Exception {
-        Util.checkNullParameter(parms == null, "parms is null");
-        Util.checkStateParameter(parms.getJobId().isEmpty(), "job id not specified");
-        Util.checkStateParameter(parms.getJobname().isEmpty(), "job name not specified");
-        Util.checkStateParameter(parms.getModifyVersion().isEmpty(), "modify version not specified");
+    public Response deleteJobCommon(DeleteJobParams params) throws Exception {
+        Util.checkNullParameter(params == null, "params is null");
+        Util.checkStateParameter(params.getJobId().isEmpty(), "job id not specified");
+        Util.checkStateParameter(params.getJobname().isEmpty(), "job name not specified");
+        Util.checkStateParameter(params.getModifyVersion().isEmpty(), "modify version not specified");
 
         String url = "https://" + connection.getHost() + ":" + connection.getPort() + JobsConstants.RESOURCE;
         LOG.debug(url);
@@ -73,17 +73,17 @@ public class DeleteJobs {
         Map<String, String> headers = new HashMap<>();
         String key, value;
 
-        if (parms.getModifyVersion().get() == "1.0") {
+        if (params.getModifyVersion().get() == "1.0") {
             key = ZosmfHeaders.HEADERS.get("X_IBM_JOB_MODIFY_VERSION_1").get(0);
             value = ZosmfHeaders.HEADERS.get("X_IBM_JOB_MODIFY_VERSION_1").get(1);
             headers.put(key, value);
-        } else if (parms.getModifyVersion().get() == "2.0") {
+        } else if (params.getModifyVersion().get() == "2.0") {
             key = ZosmfHeaders.HEADERS.get("X_IBM_JOB_MODIFY_VERSION_2").get(0);
             value = ZosmfHeaders.HEADERS.get("X_IBM_JOB_MODIFY_VERSION_2").get(1);
             headers.put(key, value);
         }
 
-        String parameters = UtilIO.FILE_DELIM + parms.getJobname().get() + UtilIO.FILE_DELIM + parms.getJobId().get();
+        String parameters = UtilIO.FILE_DELIM + params.getJobname().get() + UtilIO.FILE_DELIM + params.getJobId().get();
 
         ZoweRequest request = ZoweRequestFactory.buildRequest(connection, url + parameters, null,
                 ZoweRequestType.VerbType.DELETE_JSON);
