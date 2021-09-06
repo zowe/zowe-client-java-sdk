3399ad7e12f73fe6fcdff1ecf3376a05cf34a5c1
Frank Giordano <giofr01@yahoo.com>
2021-09-06 17:19:58 -0400

    finished adding javadoc to current code in zostso package
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zostso/zosmf/TsoMessages.java
+++ src/main/java/zostso/zosmf/TsoMessages.java
@@ -11,32 +11,85 @@ package zostso.zosmf;
 
 import java.util.Optional;
 
+/**
+ * Interface for TSO/E messages
+ *
+ * @author Frank Giordano
+ * @version 1.0
+ */
 public class TsoMessages {
 
+    /**
+     * Tso message type of TSO/E messages
+     */
     private Optional<TsoMessage> tsoMessage = Optional.empty();
+
+    /**
+     * Tso prompt message type of TSO/E messages
+     */
     private Optional<TsoPromptMessage> tsoPrompt = Optional.empty();
+
+    /**
+     * Tso response message type of TSO/E messages
+     */
     private Optional<TsoResponseMessage> tsoResponse = Optional.empty();
 
+    /**
+     * Retrieve tsoMessage specified
+     *
+     * @return tsoMessage value
+     * @author Frank Giordano
+     */
     public Optional<TsoMessage> getTsoMessage() {
         return tsoMessage;
     }
 
+    /**
+     * Assign tsoMessage value
+     *
+     * @param tsoMessage tso message value
+     * @author Frank Giordano
+     */
     public void setTsoMessage(Optional<TsoMessage> tsoMessage) {
         this.tsoMessage = tsoMessage;
     }
 
+    /**
+     * Retrieve tsoPrompt specified
+     *
+     * @return tsoPrompt value
+     * @author Frank Giordano
+     */
     public Optional<TsoPromptMessage> getTsoPrompt() {
         return tsoPrompt;
     }
 
+    /**
+     * Assign tsoPrompt value
+     *
+     * @param tsoPrompt tso prompt value
+     * @author Frank Giordano
+     */
     public void setTsoPrompt(Optional<TsoPromptMessage> tsoPrompt) {
         this.tsoPrompt = tsoPrompt;
     }
 
+    /**
+     * Retrieve tsoResponse specified
+     *
+     * @return tsoResponse value
+     * @author Frank Giordano
+     */
     public Optional<TsoResponseMessage> getTsoResponse() {
         return tsoResponse;
     }
 
+    /**
+     * Assign tsoResponse value
+     *
+     * @param tsoResponse tso response value
+     * @author Frank Giordano
+     */
     public void setTsoResponse(Optional<TsoResponseMessage> tsoResponse) {
         this.tsoResponse = tsoResponse;
     }
