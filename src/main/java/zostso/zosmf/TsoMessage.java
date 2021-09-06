5c54fdc5187c6ece04f62aae550782ace9391502
Frank Giordano <giofr01@yahoo.com>
2021-09-06 17:19:58 -0400

    finished adding javadoc to current code in zostso package
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zostso/zosmf/TsoMessage.java
+++ src/main/java/zostso/zosmf/TsoMessage.java
@@ -11,31 +11,80 @@ package zostso.zosmf;
 
 import java.util.Optional;
 
+/**
+ * TSO Message interface for one of TSO/E messages
+ *
+ * @author Frank Giordano
+ * @version 1.0
+ */
 public class TsoMessage {
 
+    /**
+     * JSON version for message format
+     */
     private Optional<String> version;
+
+    /**
+     * Description of the data type
+     */
     private Optional<String> data;
 
+    /**
+     * TsoMessage constructor
+     *
+     * @author Frank Giordano
+     */
     public TsoMessage() {
     }
 
+    /**
+     * TsoMessage constructor
+     *
+     * @param version JSON version for message format
+     * @param data    description of the data type
+     * @author Frank Giordano
+     */
     public TsoMessage(Optional<String> version, Optional<String> data) {
         this.version = version;
         this.data = data;
     }
 
+    /**
+     * Retrieve version specified
+     *
+     * @return version value
+     * @author Frank Giordano
+     */
     public Optional<String> getVersion() {
         return version;
     }
 
+    /**
+     * Assign version value
+     *
+     * @param version JSON version for message format
+     * @author Frank Giordano
+     */
     public void setVersion(Optional<String> version) {
         this.version = version;
     }
 
+    /**
+     * Retrieve data specified
+     *
+     * @return data value
+     * @author Frank Giordano
+     */
     public Optional<String> getData() {
         return data;
     }
 
+    /**
+     * Assign version value
+     *
+     * @param data description os the data type
+     * @author Frank Giordano
+     */
     public void setData(Optional<String> data) {
         this.data = data;
     }
--- src/main/java/zostso/zosmf/TsoPromptMessage.java
+++ src/main/java/zostso/zosmf/TsoPromptMessage.java
@@ -11,31 +11,80 @@ package zostso.zosmf;
 
 import java.util.Optional;
 
+/**
+ * TSO Prompt interface for one of TSO/E messages
+ *
+ * @author Frank Giordano
+ * @version 1.0
+ */
 public class TsoPromptMessage {
 
+    /**
+     * JSON version for message format
+     */
     private Optional<String> version;
+
+    /**
+     * Hidden value
+     */
     private Optional<String> hidden;
 
+    /**
+     * TsoPromptMessage constructor
+     *
+     * @author Frank Giordano
+     */
     public TsoPromptMessage() {
     }
 
+    /**
+     * TsoPromptMessage constructor
+     *
+     * @param version JSON version for message format
+     * @param hidden  hidden value
+     * @author Frank Giordano
+     */
     public TsoPromptMessage(Optional<String> version, Optional<String> hidden) {
         this.version = version;
         this.hidden = hidden;
     }
 
+    /**
+     * Retrieve version specified
+     *
+     * @return version value
+     * @author Frank Giordano
+     */
     public Optional<String> getVersion() {
         return version;
     }
 
+    /**
+     * Assign version value
+     *
+     * @param version JSON version for message format
+     * @author Frank Giordano
+     */
     public void setVersion(Optional<String> version) {
         this.version = version;
     }
 
+    /**
+     * Retrieve hidden specified
+     *
+     * @return hidden value
+     * @author Frank Giordano
+     */
     public Optional<String> getHidden() {
         return hidden;
     }
 
+    /**
+     * Assign hidden value
+     *
+     * @param hidden hidden value
+     * @author Frank Giordano
+     */
     public void setHidden(Optional<String> hidden) {
         this.hidden = hidden;
     }
