867d1c7ab944471702611c1fcd6396d305b12b11
Frank Giordano <giofr01@yahoo.com>
2023-01-19 23:25:51 -0500

    update utils

--- src/test/java/zowe/client/sdk/utility/Utils.java
+++ src/test/java/zowe/client/sdk/utility/Utils.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package zowe.client.sdk.utility;
 
 import java.lang.reflect.Constructor;
@@ -8,6 +17,11 @@ import java.lang.reflect.Modifier;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Utility class for test package.
+ *
+ * @author Frank Giordano
+ */
 public final class Utils {
 
     /**
