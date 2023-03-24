fc3d0e71e6b4289d7363c64141a373264a236c02
Frank Giordano <giofr01@yahoo.com>
2023-03-24 10:43:20 -0400

    clean up

--- src/main/java/zowe/client/sdk/utility/TsoUtils.java
+++ src/main/java/zowe/client/sdk/utility/TsoUtils.java
@@ -12,9 +12,9 @@ package zowe.client.sdk.utility;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import zowe.client.sdk.rest.Response;
-import zowe.client.sdk.zostso.response.StartStopResponse;
 import zowe.client.sdk.zostso.TsoConstants;
 import zowe.client.sdk.zostso.message.*;
+import zowe.client.sdk.zostso.response.StartStopResponse;
 
 import java.util.ArrayList;
 import java.util.List;
