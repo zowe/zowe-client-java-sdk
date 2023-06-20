5ab999412bd92290dce5a23130501e4def259a68
Frank Giordano <giofr01@yahoo.com>
2023-06-20 01:04:44 -0400

    constructed logic

--- src/main/java/zowe/client/sdk/zosfiles/dsn/methods/DsnRename.java
+++ src/main/java/zowe/client/sdk/zosfiles/dsn/methods/DsnRename.java
@@ -10,12 +10,19 @@
  */
 package zowe.client.sdk.zosfiles.dsn.methods;
 
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import zowe.client.sdk.core.ZOSConnection;
 import zowe.client.sdk.rest.Response;
 import zowe.client.sdk.rest.ZoweRequest;
+import zowe.client.sdk.rest.ZoweRequestFactory;
+import zowe.client.sdk.rest.type.ZoweRequestType;
+import zowe.client.sdk.utility.EncodeUtils;
+import zowe.client.sdk.utility.RestUtils;
 import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.utility.unirest.UniRestUtils;
+import zowe.client.sdk.zosfiles.ZosFilesConstants;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +38,7 @@ public class DsnRename {
     private static final Logger LOG = LoggerFactory.getLogger(DsnRename.class);
     private final ZOSConnection connection;
     private ZoweRequest request;
+    private String url;
 
     /**
      * DsnRename Constructor
@@ -57,21 +65,53 @@ public class DsnRename {
         this.request = request;
     }
 
-    public Response member(String dsName, String memberName, String newMemberName) {
-
-        return null;
+    public Response dataSetName(String source, String destination) throws Exception {
+        setUrl(destination);
+        return executeRequest(buildBody(source));
     }
 
-    public Response dataSet(String dsName, String newDsName) {
-
-        return null;
+    public Response memberName(String dsName, String source, String destination) throws Exception {
+        setUrl(destination);
+        return executeRequest(buildBody(dsName, source));
     }
 
-    private Response rename(String dsName) {
+    private void setUrl(String source) {
+        url = "https://" + connection.getHost() + ":" + connection.getZosmfPort()
+                + ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_DS_FILES + "/" +
+                EncodeUtils.encodeURIComponent(source);
+    }
+
+    private String buildBody(String... args) {
         final Map<String, Object> jsonMap = new HashMap<>();
         jsonMap.put("request", "rename");
 
-        return null;
+        final Map<String, Object> fromDataSetReq = new HashMap<>();
+        fromDataSetReq.put("dsn", args[0]);
+        if (args.length > 1) {
+            fromDataSetReq.put("member", args[1]);
+        }
+
+        final JSONObject fromDataSetObj = new JSONObject(fromDataSetReq);
+        jsonMap.put("from-dataset", fromDataSetObj);
+
+        final JSONObject jsonRequestBody = new JSONObject(jsonMap);
+        LOG.debug(String.valueOf(jsonRequestBody));
+        return jsonRequestBody.toString();
+    }
+
+    private Response executeRequest(String body) throws Exception {
+        if (request == null) {
+            request = ZoweRequestFactory.buildRequest(connection, ZoweRequestType.PUT_JSON);
+        }
+        request.setUrl(url);
+        request.setBody(body);
+
+        final Response response = UniRestUtils.getResponse(request);
+        if (RestUtils.isHttpError(response.getStatusCode().get())) {
+            throw new Exception(response.getResponsePhrase().get().toString());
+        }
+
+        return response;
     }
 
 }
