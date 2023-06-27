8bcf48afb1d57d3b5682eae291cd97277bd34b43
James Kostrewski <105880605+kostrja@users.noreply.github.com>
2023-06-27 11:33:24 -0400

    added UssWrite

--- src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssWrite.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssWrite.java
@@ -9,5 +9,97 @@
  */
 package zowe.client.sdk.zosfiles.uss.methods;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.*;
+import zowe.client.sdk.rest.type.ZoweRequestType;
+import zowe.client.sdk.utility.RestUtils;
+import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.zosfiles.ZosFilesConstants;
+
+/**
+ * Provides unix system service write to object functionality
+ * <p>
+ * <a href="https://www.ibm.com/docs/en/zos/2.4.0?topic=interface-write-data-zos-unix-file">z/OSMF REST API</a>
+ * 
+ * @author James Kostrewski
+ * @version 2.0
+ */
 public class UssWrite {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UssDelete.class);
+    private final ZosConnection connection;
+    private ZoweRequest request;
+
+    /**
+     * UssWrite Constructor
+     *
+     * @param connection connection information, see ZOSConnection object
+     * @author James Kostrewski
+     */
+    public UssWrite(ZosConnection connection) {
+        ValidateUtils.checkConnection(connection);
+        this.connection = connection;
+    }
+
+    /**
+     * Alternative UssWrite constructor with ZoweRequest object. This is mainly used for internal code
+     * unit testing with mockito, and it is not recommended to be used by the larger community.
+     *
+     * @param connection connection information, see ZOSConnection object
+     * @param request    any compatible ZoweRequest Interface type object
+     * @author James Kostrewski
+     * @author Frank Giordano
+     */
+    public UssWrite(ZosConnection connection, ZoweRequest request) {
+        ValidateUtils.checkConnection(connection);
+        this.connection = connection;
+        this.request = request;
+    }
+    
+    /**
+     * Replaces the content of a unix file with new content.
+     *
+     * @param destName destination name of where the file is located
+     * @param memberName  name of member to add new content
+     * @param content     new content
+     * @return http response object
+     * @throws Exception error processing request
+     * @author Frank Giordano
+     */
+    public Response write(String destName, String memberName, String content) throws Exception {
+        ValidateUtils.checkNullParameter(destName == null, "destName is null");
+        ValidateUtils.checkIllegalParameter(destName.isEmpty(), "dataSetName not specified");
+        ValidateUtils.checkNullParameter(memberName == null, "memberName is null");
+        ValidateUtils.checkIllegalParameter(memberName.isEmpty(), "memberName not specified");
+        return write(String.format("%s(%s)", destName, memberName), content);
+    }
+    
+    /**
+     * Perform a write to UNIX object request
+     *
+     * @param destName name of file or directory with path
+     * @param content new content
+     * @return http response object
+     * @throws Exception error processing request
+     * @author James Kostrewski
+     */
+    public Response write(String destName, String content) throws Exception {
+        ValidateUtils.checkNullParameter(destName == null, "destName is null");
+        ValidateUtils.checkNullParameter(content == null, "content is null");
+
+        final String url = "https://" + connection.getHost() + ":" + connection.getZosmfPort() +
+                ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES + destName;
+        LOG.debug(url);
+
+        if (request == null || !(request instanceof JsonDeleteRequest)) {
+            request = ZoweRequestFactory.buildRequest(connection, ZoweRequestType.PUT_JSON);
+        }
+        request.setUrl(url);
+        request.setBody(content);
+
+        return RestUtils.getResponse(request);
+    }
+
 }
