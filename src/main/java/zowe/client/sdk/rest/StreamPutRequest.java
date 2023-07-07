b50fc5295d55ed13fd2464134a7bb53bcf5b4912
Frank Giordano <giofr01@yahoo.com>
2023-07-06 23:52:33 -0400

    add header info

--- src/main/java/zowe/client/sdk/rest/StreamPutRequest.java
+++ src/main/java/zowe/client/sdk/rest/StreamPutRequest.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package zowe.client.sdk.rest;
 
 import kong.unirest.HttpResponse;
@@ -8,6 +17,12 @@ import zowe.client.sdk.core.ZosConnection;
 import zowe.client.sdk.utility.EncodeUtils;
 import zowe.client.sdk.utility.ValidateUtils;
 
+/**
+ * Http put stream operation with binary content type
+ *
+ * @author Frank Giordano
+ * @version 2.0
+ */
 public class StreamPutRequest extends ZoweRequest {
 
     /**
