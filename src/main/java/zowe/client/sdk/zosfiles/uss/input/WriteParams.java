c784f757d686119805da6216ecceabf81efa3b28
frankgiordano <giofr01@yahoo.com>
2023-07-09 23:30:09 -0400

    fill out writeparms

--- src/main/java/zowe/client/sdk/zosfiles/uss/input/WriteParams.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/input/WriteParams.java
@@ -17,17 +17,108 @@ import java.util.Optional;
  * <a href="https://www.ibm.com/docs/en/zos/2.4.0?topic=interface-write-data-zos-unix-file">z/OSMF REST API</a>
  *
  * @author Frank Giordano
- * @author James Kostrewski
  * @version 2.0
  */
 public class WriteParams {
 
+    /**
+     * Content text string for file write
+     */
     public Optional<String> textContent;
+    /**
+     * Binary byte array content for file write
+     */
     public Optional<byte[]> binaryContent;
+    /**
+     * Can be used to specify an alternate EBCDIC code page. The default code page is IBM-1047.
+     */
     public Optional<String> fileEncoding;
-    public Optional<String> crlf;
+    /**
+     *  Can be used to control whether each input text line is terminated with a carriage return line feed (CRLF),
+     *  rather than a line feed (LF), which is the default. Set to true to turn off default.
+     */
+    public boolean crlf;
+    /**
+     * If true perform binary write instead of text.
+     */
     public boolean binary;
 
-    // TODO build builder pattern below...
+    public WriteParams(WriteParams.Builder builder) {
+        this.textContent = Optional.ofNullable(builder.textContent);
+        this.binaryContent = Optional.ofNullable(builder.binaryContent);
+        this.fileEncoding = Optional.ofNullable(builder.fileEncoding);
+        this.crlf = builder.crlf;
+        this.binary = builder.binary;
+    }
+
+    public Optional<String> getTextContent() {
+        return textContent;
+    }
+
+    public Optional<byte[]> getBinaryContent() {
+        return binaryContent;
+    }
+
+    public Optional<String> getFileEncoding() {
+        return fileEncoding;
+    }
+
+    public boolean isCrlf() {
+        return crlf;
+    }
+
+    public boolean isBinary() {
+        return binary;
+    }
+
+    @Override
+    public String toString() {
+        return "WriteParams{" +
+                "textContent=" + textContent +
+                ", binaryContent=" + binaryContent +
+                ", fileEncoding=" + fileEncoding +
+                ", crlf=" + crlf +
+                ", binary=" + binary +
+                '}';
+    }
+
+    public static class Builder {
+
+        private String textContent;
+        private byte[] binaryContent;
+        private String fileEncoding;
+        private boolean crlf = false;
+        private boolean binary = false;
+
+        public WriteParams build() {
+            return new WriteParams(this);
+        }
+
+        public WriteParams.Builder textContent(String textContent) {
+            this.textContent = textContent;
+            return this;
+        }
+
+        public WriteParams.Builder binaryContent(byte[] binaryContent) {
+            this.binaryContent = binaryContent;
+            return this;
+        }
+
+        public WriteParams.Builder fileEncoding(String fileEncoding) {
+            this.fileEncoding = fileEncoding;
+            return this;
+        }
+
+        public WriteParams.Builder crlf(boolean crlf) {
+            this.crlf = crlf;
+            return this;
+        }
+
+        public WriteParams.Builder binary(boolean binary) {
+            this.binary = binary;
+            return this;
+        }
+
+    }
 
 }
