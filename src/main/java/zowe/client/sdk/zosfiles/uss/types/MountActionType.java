ac3993afc94c0363fd582fdd2224ce04ffc6458c
Frank Giordano <giofr01@yahoo.com>
2023-07-18 13:33:37 -0400

    Create MountActionType.java related to #279
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

---
+++ src/main/java/zowe/client/sdk/zosfiles/uss/types/MountActionType.java
@@ -0,0 +1,35 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package zowe.client.sdk.zosfiles.uss.types;
+
+/**
+ * MountActionType class provides type representation of Unix System Services (USS) mount action
+ * <p>
+ * <a href="https://www.ibm.com/docs/en/zos/2.4.0?topic=interface-mount-zos-unix-file-system">z/OSMF REST API</a>
+ *
+ * @author Frank Giordano
+ * @version 2.0
+ */
+public enum MountActionType {
+
+    MOUNT("mount"),
+    UNMOUNT("unmount");
+
+    private final String value;
+
+    MountActionType(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
