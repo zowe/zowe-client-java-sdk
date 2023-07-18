8d4573b172d3b7528977af30764217bbe0545492
Frank Giordano <giofr01@yahoo.com>
2023-07-18 11:02:40 -0400

    Update UssMount.java - scaffolding for #279
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssMount.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssMount.java
@@ -9,5 +9,89 @@
  */
 package zowe.client.sdk.zosfiles.uss.methods;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.Response;
+import zowe.client.sdk.rest.ZoweRequest;
+import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.zosfiles.uss.input.MountParams;
+
+/**
+ * Provides Unix System Services (USS) mount and unmount of a fil system name
+ * <p>
+ * <a href="https://www.ibm.com/docs/en/zos/2.4.0?topic=interface-mount-zos-unix-file-system">z/OSMF REST MOUNT API</a>
+ * <a href="https://www.ibm.com/docs/en/zos/2.4.0?topic=interface-unmount-unix-file-system">z/OSMF REST UNMOUNT API</a>
+ *
+ * @author Frank Giordano
+ * @version 2.0
+ */
 public class UssMount {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UssMount.class);
+    private final ZosConnection connection;
+    private ZoweRequest request;
+
+    /**
+     * UssMount Constructor
+     *
+     * @param connection connection information, see ZosConnection object
+     * @author Frank Giordano
+     */
+    public UssMount(ZosConnection connection) {
+        ValidateUtils.checkConnection(connection);
+        this.connection = connection;
+    }
+
+    /**
+     * Alternative UssMount constructor with ZoweRequest object. This is mainly used for internal code
+     * unit testing with mockito, and it is not recommended to be used by the larger community.
+     *
+     * @param connection connection information, see ZosConnection object
+     * @param request    any compatible ZoweRequest Interface object
+     * @author Frank Giordano
+     */
+    public UssMount(ZosConnection connection, ZoweRequest request) {
+        ValidateUtils.checkConnection(connection);
+        this.connection = connection;
+        this.request = request;
+    }
+
+    /**
+     * Perform mount of a file system to the USS file system
+     *
+     * @param fileSystemName the file system that you want to mount
+     * @param mountPoint     the mount point to be used for mounting the UNIX file system
+     * @return Response object
+     * @author Frank Giordano
+     */
+    public Response mount(String fileSystemName, String mountPoint) {
+
+        return null;
+    }
+
+    /**
+     * Perform unmount of a file system
+     *
+     * @param fileSystemName the file system that you want to unmount
+     * @return Response object
+     * @author Frank Giordano
+     */
+    public Response unMount(String fileSystemName) {
+
+        return null;
+    }
+
+    /**
+     * Perform mount or unmount request driven by MountParams settings
+     *
+     * @param fileSystemName the file system that you want to mount or unmount
+     * @return Response object
+     * @author Frank Giordano
+     */
+    public Response mountCommon(String fileSystemName, MountParams params) {
+
+        return null;
+    }
+
 }
