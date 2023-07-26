566153a71fea9de250ec6518dc2f8f2105921ba5
Frank Giordano <giofr01@yahoo.com>
2023-07-26 15:11:59 -0400

    Update IssueConsole.java - guard against null request
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/zosconsole/method/IssueConsole.java
+++ src/main/java/zowe/client/sdk/zosconsole/method/IssueConsole.java
@@ -66,6 +66,7 @@ public class IssueConsole {
      */
     public IssueConsole(ZosConnection connection, ZoweRequest request) throws Exception {
         ValidateUtils.checkConnection(connection);
+        ValidateUtils.checkNullParameter(request == null, "request is null");
         this.connection = connection;
         if (!(request instanceof JsonPutRequest)) {
             throw new Exception("PUT_JSON request type required");
