8cc2368e718a57a0796f770a3688da4c8b244360
Frank Giordano <giofr01@yahoo.com>
2023-07-26 15:16:10 -0400

    Update SendTso.java - guard against null request
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/zostso/lifecycle/SendTso.java
+++ src/main/java/zowe/client/sdk/zostso/lifecycle/SendTso.java
@@ -65,6 +65,7 @@ public class SendTso {
      */
     public SendTso(ZosConnection connection, ZoweRequest request) throws Exception {
         ValidateUtils.checkConnection(connection);
+        ValidateUtils.checkNullParameter(request == null, "request is null");
         this.connection = connection;
         if (!(request instanceof JsonPutRequest)) {
             throw new Exception("PUT_JSON request type required");
