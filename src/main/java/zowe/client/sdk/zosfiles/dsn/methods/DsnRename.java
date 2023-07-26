dd7c023c7afbdef538a535200246f4bc91db0567
Frank Giordano <giofr01@yahoo.com>
2023-07-26 15:18:27 -0400

    Update DsnRename.java - add missing import
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/zosfiles/dsn/methods/DsnRename.java
+++ src/main/java/zowe/client/sdk/zosfiles/dsn/methods/DsnRename.java
@@ -14,6 +14,7 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.JsonPutRequest;
 import zowe.client.sdk.rest.Response;
 import zowe.client.sdk.rest.ZoweRequest;
 import zowe.client.sdk.rest.ZoweRequestFactory;
