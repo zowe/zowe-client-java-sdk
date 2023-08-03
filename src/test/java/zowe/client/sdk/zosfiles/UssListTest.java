882fb5e5eb793b8c041f7a782fcadd7e740f82d7
Frank Giordano <giofr01@yahoo.com>
2023-08-03 13:31:53 -0400

    Update UssListTest.java - added mock tests
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/test/java/zowe/client/sdk/zosfiles/UssListTest.java
+++ src/test/java/zowe/client/sdk/zosfiles/UssListTest.java
@@ -9,12 +9,21 @@
  */
 package zowe.client.sdk.zosfiles;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.JsonGetRequest;
+import zowe.client.sdk.rest.Response;
 import zowe.client.sdk.zosfiles.uss.input.ListParams;
 import zowe.client.sdk.zosfiles.uss.input.ListZfsParams;
 import zowe.client.sdk.zosfiles.uss.methods.UssList;
+import zowe.client.sdk.zosfiles.uss.response.UssItem;
+import zowe.client.sdk.zosfiles.uss.response.UssZfsItem;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,12 +36,143 @@ import static org.junit.Assert.assertEquals;
 public class UssListTest {
 
     private ZosConnection connection;
+    private JsonGetRequest jsonGetRequest;
     private UssList ussList;
+    private final static String dataForFileList = "{\n" +
+            "   \"items\": [\n" +
+            "      {\n" +
+            "         \"name\": \"test\",\n" +
+            "         \"mode\": \"drwxr-xr-x\",\n" +
+            "         \"size\": 0,\n" +
+            "         \"uid\": 10000518,\n" +
+            "         \"user\": \"user\",\n" +
+            "         \"gid\": 8,\n" +
+            "         \"group\": \"FRAMEWKG\",\n" +
+            "         \"mtime\": \"2022-11-03T10:48:32\"\n" +
+            "      },\n" +
+            "      {\n" +
+            "         \"name\": \"test2\",\n" +
+            "         \"mode\": \"-rwxr-xr-x\",\n" +
+            "         \"size\": 13545,\n" +
+            "         \"uid\": 10000518,\n" +
+            "         \"user\": \"user2\",\n" +
+            "         \"gid\": 8,\n" +
+            "         \"group\": \"FRAMEWKG\",\n" +
+            "         \"mtime\": \"2022-11-12T15:20:11\"\n" +
+            "      }\n" +
+            "   ],\n" +
+            "   \"returnedRows\": 7,\n" +
+            "   \"totalRows\": 7,\n" +
+            "   \"JSONversion\": 1\n" +
+            "}";
+    private final static String dataForZfsList = "{\n" +
+            "   \"items\": [\n" +
+            "      {\n" +
+            "         \"name\": \"OMVSGRP.USER.TNGFW.CA31\",\n" +
+            "         \"mountpoint\": \"/CA31/u/users/framewrk\",\n" +
+            "         \"fstname\": \"ZFS\",\n" +
+            "         \"status\": \"active\",\n" +
+            "         \"mode\": [\n" +
+            "            \"noautomove\",\n" +
+            "            \"unmount\",\n" +
+            "            \"acl\",\n" +
+            "            \"synchonly\"\n" +
+            "         ],\n" +
+            "         \"dev\": 2718,\n" +
+            "         \"fstype\": 1,\n" +
+            "         \"bsize\": 1024,\n" +
+            "         \"bavail\": 269231,\n" +
+            "         \"blocks\": 1382400,\n" +
+            "         \"sysname\": \"CA31\",\n" +
+            "         \"readibc\": 907651,\n" +
+            "         \"writeibc\": 42,\n" +
+            "         \"diribc\": 453057\n" +
+            "      }\n" +
+            "   ],\n" +
+            "   \"JSONversion\": 1\n" +
+            "}";
 
     @Before
     public void init() {
         connection = new ZosConnection("1", "1", "1", "1");
+        jsonGetRequest = Mockito.mock(JsonGetRequest.class);
         ussList = new UssList(connection);
+    }
+
+    @Test
+    public void tstUssListFileListSuccess() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(dataForFileList);
+        Mockito.when(jsonGetRequest.executeRequest()).thenReturn(
+                new Response(json, 200, "success"));
+        UssList ussList = new UssList(connection, jsonGetRequest);
+        List<UssItem> items = ussList.fileList(new ListParams.Builder().path("/xxx/xx/x").build());
+        // should only contain two items
+        assertEquals(2, items.size());
+        // verify first item's data
+        assertEquals("test", items.get(0).getName().get());
+        assertEquals("drwxr-xr-x", items.get(0).getMode().get());
+        assertEquals(0, items.get(0).getSize().getAsLong());
+        assertEquals(10000518, items.get(0).getUid().getAsLong());
+        assertEquals("user", items.get(0).getUser().get());
+        assertEquals(8, items.get(0).getGid().getAsLong());
+        assertEquals("FRAMEWKG", items.get(0).getGroup().get());
+        assertEquals("2022-11-03T10:48:32", items.get(0).getMtime().get());
+        // verify second item's data
+        assertEquals("test2", items.get(1).getName().get());
+        assertEquals("-rwxr-xr-x", items.get(1).getMode().get());
+        assertEquals(13545, items.get(1).getSize().getAsLong());
+        assertEquals(10000518, items.get(1).getUid().getAsLong());
+        assertEquals("user2", items.get(1).getUser().get());
+        assertEquals(8, items.get(1).getGid().getAsLong());
+        assertEquals("FRAMEWKG", items.get(1).getGroup().get());
+        assertEquals("2022-11-12T15:20:11", items.get(1).getMtime().get());
+    }
+
+    @Test
+    public void tstUssListEmptyFileListSuccess() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse("{}");
+        Mockito.when(jsonGetRequest.executeRequest()).thenReturn(
+                new Response(json, 200, "success"));
+        UssList ussList = new UssList(connection, jsonGetRequest);
+        List<UssItem> items = ussList.fileList(new ListParams.Builder().path("/xxx/xx/x").build());
+        assertEquals(0, items.size());
+    }
+
+    @Test
+    public void tstUssListEmptyFileListWithJsonObjectSuccess() throws Exception {
+        Mockito.when(jsonGetRequest.executeRequest()).thenReturn(
+                new Response(new JSONObject(), 200, "success"));
+        UssList ussList = new UssList(connection, jsonGetRequest);
+        List<UssItem> items = ussList.fileList(new ListParams.Builder().path("/xxx/xx/x").build());
+        assertEquals(0, items.size());
+    }
+
+    @Test
+    public void tstUssListZfsListSuccess() throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject json = (JSONObject) parser.parse(dataForZfsList);
+        Mockito.when(jsonGetRequest.executeRequest()).thenReturn(
+                new Response(json, 200, "success"));
+        UssList ussList = new UssList(connection, jsonGetRequest);
+        List<UssZfsItem> items = ussList.zfsList(new ListZfsParams.Builder().path("/xxx/xx/x").build());
+        // should only contain two items
+        assertEquals(1, items.size());
+        // verify first item's data
+        assertEquals("OMVSGRP.USER.TNGFW.CA31", items.get(0).getName().get());
+        assertEquals("/CA31/u/users/framewrk", items.get(0).getMountpoint().get());
+        assertEquals("ZFS", items.get(0).getFstname().get());
+        assertEquals("noautomove, unmount, acl, synchonly", items.get(0).getMode().get());
+        assertEquals(2718, items.get(0).getDev().getAsLong());
+        assertEquals(1, items.get(0).getFstype().getAsLong());
+        assertEquals(1024, items.get(0).getBsize().getAsLong());
+        assertEquals(269231, items.get(0).getBavail().getAsLong());
+        assertEquals(1382400, items.get(0).getBlocks().getAsLong());
+        assertEquals("CA31", items.get(0).getSysname().get());
+        assertEquals(907651, items.get(0).getReadibc().getAsLong());
+        assertEquals(42, items.get(0).getWriteibc().getAsLong());
+        assertEquals(453057, items.get(0).getDiribc().getAsLong());
     }
 
     @Test
