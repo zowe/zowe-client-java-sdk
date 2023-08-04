998e165d5667f860c8a8011121b8ce343bb10c5b
Frank Giordano <giofr01@yahoo.com>
2023-08-04 14:52:48 -0400

    Update UssGet.java
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssGet.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssGet.java
@@ -110,7 +110,7 @@ public class UssGet {
 
         final StringBuilder url = new StringBuilder("https://" + connection.getHost() + ":" + connection.getZosmfPort() +
                 ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES + 
-                EncodeUtils.encodeURIComponent(FileUtils.validatePath(fileNamePath));
+                EncodeUtils.encodeURIComponent(FileUtils.validatePath(fileNamePath)));
 
         params.getSearch().ifPresent(str -> url.append("?search=").append(EncodeUtils.encodeURIComponent(str)));
         params.getResearch().ifPresent(str -> url.append("?research=").append(EncodeUtils.encodeURIComponent(str)));
