0025a8489fd455231e3c30cbb23ace0fb548377c
Frank Giordano <giofr01@yahoo.com>
2023-08-10 19:55:37 -0400

    Update RestUtils.java - massaging tweaks
    Signed-off-by: Frank Giordano <giofr01@yahoo.com>

--- src/main/java/zowe/client/sdk/utility/RestUtils.java
+++ src/main/java/zowe/client/sdk/utility/RestUtils.java
@@ -19,7 +19,6 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
@@ -52,32 +51,25 @@ public final class RestUtils {
         final int statusCode = response.getStatusCode().orElseThrow(() -> new Exception(statusCodeErrMsg));
 
         if (RestUtils.isHttpError(statusCode)) {
-            final AtomicReference<Object> responsePhase = new AtomicReference<>();
-            response.getResponsePhrase().ifPresent(phrase -> responsePhase.set(phrase));
-            if (responsePhase.get() instanceof byte[]) {
+            final AtomicReference<Object> responsePhrase = new AtomicReference<>();
+            response.getResponsePhrase().ifPresent(phrase -> responsePhrase.set(phrase));
+            if (responsePhrase.get() instanceof byte[]) {
                 final InputStreamReader inputStreamReader = new InputStreamReader(
-                        new ByteArrayInputStream((byte[]) responsePhase.get()), StandardCharsets.UTF_8);
+                        new ByteArrayInputStream((byte[]) responsePhrase.get()), StandardCharsets.UTF_8);
                 final BufferedReader br = new BufferedReader(inputStreamReader);
                 String content = "";
                 String line;
                 while ((line = br.readLine()) != null) {
                     content += line + "\n";
                 }
-                responsePhase.set(content.substring(0, content.length() - 1));
+                responsePhrase.set(content.substring(0, content.length() - 1));
             }
 
-            final Optional<String> responsePhaseStr = Optional.ofNullable(responsePhase.get().toString());
-            final String responsePhraseErrMsg = "no response phrase returned";
-            final String responsePhrase = String.valueOf((
-                    !responsePhaseStr.isEmpty() && !responsePhaseStr.get().trim().isEmpty() ?
-                            responsePhase.get() : responsePhraseErrMsg));
-            final String statusTextErrMsg = "no response status text returned";
-            final String statusText =
-                    !response.getStatusText().isEmpty() && !response.getStatusText().get().trim().isEmpty()
-                    ? response.getStatusText().get() : statusTextErrMsg;
+            final String responsePhraseStr = String.valueOf((responsePhrase.get()));
+            final String statusText = !response.getStatusText().isEmpty() ? response.getStatusText().get() : "n\\a";
             String errMsg = "http status error code: " + statusCode + ", status text: " + statusText;
-            if (!statusText.equalsIgnoreCase(responsePhrase)) {
-                errMsg += ", response phrase: " + responsePhrase;
+            if (!statusText.equalsIgnoreCase(responsePhraseStr)) {
+                errMsg += ", response phrase: " + responsePhraseStr;
             }
             throw new Exception(errMsg);
         }
