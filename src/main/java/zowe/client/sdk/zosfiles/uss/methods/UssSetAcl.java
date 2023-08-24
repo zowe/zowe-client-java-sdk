1260c5e703ba123b2e2796e07b29ebfa2fa64918
James Kostrewski <105880605+kostrja@users.noreply.github.com>
2023-08-24 16:37:42 -0400

    Update UssSetAcl.java

--- src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssSetAcl.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssSetAcl.java
@@ -21,6 +21,7 @@ import zowe.client.sdk.utility.FileUtils;
 import zowe.client.sdk.utility.ValidateUtils;
 import zowe.client.sdk.zosfiles.ZosFilesConstants;
 import zowe.client.sdk.zosfiles.uss.input.SetAclParams;
+import zowe.client.sdk.zosfiles.uss.types.DeleteType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,6 +43,7 @@ public class UssSetAcl {
      * UssSetAcl Constructor
      *
      * @param connection connection information, see ZosConnection object
+     * @author James Kostrewski
      */
     public UssSetAcl(ZosConnection connection) {
         ValidateUtils.checkConnection(connection);
@@ -53,8 +55,9 @@ public class UssSetAcl {
      * with mockito, and it is not recommended to be used by the larger community.
      *
      * @param connection connection information, see ZosConnection object
-     * @param request any compatible ZoweRequest Interface object
+     * @param request    any compatible ZoweRequest Interface object
      * @throws Exception processing error
+     * @author James Kostrewski
      */
     public UssSetAcl(ZosConnection connection, ZosmfRequest request) throws Exception {
         ValidateUtils.checkConnection(connection);
@@ -70,14 +73,64 @@ public class UssSetAcl {
      * Sets the ACL for a USS file or directory
      *
      * @param targetPath target path of the file or directory
-     * @param params GetAclParams object to drive the request
-     * @return String representation of facl response phrase
-     * @throws Exception processing error
+     * @param value      sets the extended ACL entries that are specified by 'entries'
+     * @return Response object
+     * @author James Kostrewski
      */
-    public Response setAcl(String targetPath, SetAclParams params) {
+    public Response set(String targetPath, String value) {
+        return common(targetPath, new SetAclParams.Builder().setSet(value).build());
+    }
+
+    /**
+     * Modifies the specified ACL entry for the file or directory
+     *
+     * @param targetPath target path of the file or directory
+     * @param value      modifies the extended ACL entries that are specified by 'entries'
+     * @return Response object
+     * @author James Kostrewski
+     */
+    public Response modify(String targetPath, String value) {
+        return common(targetPath, new SetAclParams.Builder().setModify(value).build());
+    }
+
+    /**
+     * Deletes the specified ACL entry from the file or directory
+     *
+     * @param targetPath target path of the file or directory
+     * @param value      deletes the extended ACL entries that are specified by 'entries'
+     * @return Response object
+     * @author James Kostrewski
+     */
+    public Response delete(String targetPath, String value) {
+        return common(targetPath, new SetAclParams.Builder().setDelete(value).build());
+    }
+
+    /**
+     * Delete all extended ACL entries by type (setfacl -D type):
+     *
+     * @param targetPath target path of the file or directory
+     * @param deleteType deletes the extended ACL entries that are specified by type
+     * @author James Kostrewski
+     */
+    public Response deleteByType(String targetPath, DeleteType deleteType) {
+        return common(targetPath, new SetAclParams.Builder().setDeleteType(deleteType).build());
+    }
+
+    /**
+     * Sets the ACL for a USS file or directory
+     *
+     * @param targetPath target path of the file or directory
+     * @param params     GetAclParams object to drive the request
+     * @return Response object
+     * @author James Kostrewski
+     */
+    public Response common(String targetPath, SetAclParams params) {
         ValidateUtils.checkNullParameter(targetPath == null, "targetPath is null");
         ValidateUtils.checkIllegalParameter(targetPath.isBlank(), "targetPath not specified");
         ValidateUtils.checkNullParameter(params == null, "params is null");
+        ValidateUtils.checkIllegalParameter(
+                params.getSet().isEmpty() && params.getModify().isEmpty() && params.getDelete().isEmpty() &&
+                        params.getDeleteType().isEmpty(), "set, modify, delete, and delete type are all empty");
 
         final String url = "https://" + connection.getHost() + ":" + connection.getZosmfPort() +
                 ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES +
@@ -85,7 +138,9 @@ public class UssSetAcl {
 
         final Map<String, Object> setMap = new HashMap<>();
         setMap.put("request", "setfacl");
-        setMap.put("abort", params.isAbort());
+        if (params.isAbort()) {
+            setMap.put("abort", params.isAbort());
+        }
         params.getLinks().ifPresent(links -> setMap.put("links", links.getValue()));
         params.getDeleteType().ifPresent(deleteType -> setMap.put("delete-type", deleteType.getValue()));
         params.getSet().ifPresent(set -> setMap.put("set", set));
@@ -100,6 +155,5 @@ public class UssSetAcl {
 
         return request.executeRequest();
     }
-
 
 }
