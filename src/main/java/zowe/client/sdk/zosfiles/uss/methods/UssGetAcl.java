2f0edc8a7facbb3ef80748b6ac1511cc16cec983
James Kostrewski <105880605+kostrja@users.noreply.github.com>
2023-08-24 16:37:44 -0400

    Update UssGetAcl.java

--- src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssGetAcl.java
+++ src/main/java/zowe/client/sdk/zosfiles/uss/methods/UssGetAcl.java
@@ -37,99 +37,105 @@ import java.util.Map;
  */
 public class UssGetAcl {
 
-        private final ZosConnection connection;
-        private ZosmfRequest request;
+    private final ZosConnection connection;
+    private ZosmfRequest request;
 
-        /**
-        * UssGetAcl Constructor
-        *
-        * @param connection connection information, see ZosConnection object
-        */
-        public UssGetAcl(ZosConnection connection) {
-            ValidateUtils.checkConnection(connection);
-            this.connection = connection;
-        }
+    /**
+     * UssGetAcl Constructor
+     *
+     * @param connection connection information, see ZosConnection object
+     * @author James Kostrewski
+     */
+    public UssGetAcl(ZosConnection connection) {
+        ValidateUtils.checkConnection(connection);
+        this.connection = connection;
+    }
 
-        /**
-        * Alternative UssGetAcl constructor with ZoweRequest object. This is mainly used for internal code unit testing
-        * with mockito, and it is not recommended to be used by the larger community.
-        *
-        * @param connection connection information, see ZosConnection object
-        * @param request any compatible ZoweRequest Interface object
-        * @throws Exception processing error
-        */
-        public UssGetAcl(ZosConnection connection, ZosmfRequest request) throws Exception {
-            ValidateUtils.checkConnection(connection);
-            ValidateUtils.checkNullParameter(request == null, "request is null");
-            this.connection = connection;
-            if (!(request instanceof PutJsonZosmfRequest)) {
-                throw new Exception("PUT_JSON request type required");
-            }
-            this.request = request;
+    /**
+     * Alternative UssGetAcl constructor with ZoweRequest object. This is mainly used for internal code unit testing
+     * with mockito, and it is not recommended to be used by the larger community.
+     *
+     * @param connection connection information, see ZosConnection object
+     * @param request    any compatible ZoweRequest Interface object
+     * @throws Exception processing error
+     * @author James Kostrewski
+     */
+    public UssGetAcl(ZosConnection connection, ZosmfRequest request) throws Exception {
+        ValidateUtils.checkConnection(connection);
+        ValidateUtils.checkNullParameter(request == null, "request is null");
+        this.connection = connection;
+        if (!(request instanceof PutJsonZosmfRequest)) {
+            throw new Exception("PUT_JSON request type required");
         }
+        this.request = request;
+    }
+
 
     /**
      * Get the ACL for a USS file or directory
      *
-     * @param fileNamePath file name with path
-     * @param commas true if commas are to be used in the output
-     * @return String representation of facl response phrase
+     * @param targetPath file name with path
+     * @param commas     true if commas are to be used in the output
+     * @return String representation of response phrase
      * @throws Exception processing error
+     * @author James Kostrewski
      */
-    public String getAcl(String fileNamePath, boolean commas) throws Exception {
-            Response response = getAclCommon(fileNamePath, new GetAclParams.Builder().build());
-            final JSONParser parser = new JSONParser();
-            final JSONObject json = (JSONObject) parser.parse(response.getResponsePhrase().get().toString());
-            JSONArray jsonArray = (JSONArray) json.get("stdout");
-            StringBuilder sb = new StringBuilder();
-            if (commas)
-                jsonArray.forEach(item -> sb.append(item.toString()));
-            else
-                jsonArray.forEach(item -> sb.append(item.toString() + "\n"));
-
-            return sb.toString();
+    public String get(String targetPath, boolean commas) throws Exception {
+        Response response;
+        if (commas) {
+            response = getCommon(targetPath, new GetAclParams.Builder().useCommas(true).build());
+        } else {
+            response = getCommon(targetPath, new GetAclParams.Builder().build());
         }
-
-        /**
-        * Get the ACL for a USS file or directory
-        *
-        * @param fileNamePath file name with path
-         * @param params GetAclParams object to drive the request
-        * @return Response object
-        * @throws Exception processing error
-        */
-        public Response getAclCommon(String fileNamePath, GetAclParams params) throws Exception {
-            ValidateUtils.checkNullParameter(fileNamePath == null, "path is null");
-            ValidateUtils.checkIllegalParameter(fileNamePath.trim().isBlank(), "path is empty");
-            ValidateUtils.checkNullParameter(params == null, "params is null");
-
-            final String url = "https://" + connection.getHost() + ":" + connection.getZosmfPort() +
-                    ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES +
-                    EncodeUtils.encodeURIComponent(FileUtils.validatePath(fileNamePath));
-
-            final Map<String, Object> getFaclMap = new HashMap<>();
-            getFaclMap.put("request", "getfacl");
-            params.getType().ifPresent(type -> getFaclMap.put("type", type));
-            params.getUser().ifPresent(user -> getFaclMap.put("user", user));
-            if (params.getUseCommas())
-                getFaclMap.put("use-commas", params.getUseCommas());
-            if (params.getSuppressHeader())
-                getFaclMap.put("suppress-header", params.getSuppressHeader());
-            if (params.getSuppressBaseAcl())
-                getFaclMap.put("suppress-baseacl", params.getSuppressBaseAcl());
-
-            if (request == null) {
-                request = ZosmfRequestFactory.buildRequest(connection, ZosmfRequestType.PUT_JSON);
-            }
-            request.setUrl(url);
-            request.setBody(new JSONObject(getFaclMap).toString());
-
-            Response response = request.executeRequest();
-
-            return response;
-
+        final JSONParser parser = new JSONParser();
+        final JSONObject json = (JSONObject) parser.parse(response.getResponsePhrase().get().toString());
+        JSONArray jsonArray = (JSONArray) json.get("stdout");
+        StringBuilder sb = new StringBuilder();
+        if (commas) {
+            jsonArray.forEach(item -> sb.append(item.toString()));
+        } else {
+            jsonArray.forEach(item -> sb.append(item.toString() + "\n"));
         }
+        return sb.toString();
+    }
 
+    /**
+     * Get the ACL for a USS file or directory
+     *
+     * @param targetPath file name with path
+     * @param params     GetAclParams object to drive the request
+     * @return Response object
+     * @throws Exception processing error
+     * @author James Kostrewski
+     */
+    public Response getCommon(String targetPath, GetAclParams params) {
+        ValidateUtils.checkNullParameter(targetPath == null, "targetPath is null");
+        ValidateUtils.checkIllegalParameter(targetPath.isBlank(), "path is empty");
+        ValidateUtils.checkNullParameter(params == null, "params is null");
 
+        final String url = "https://" + connection.getHost() + ":" + connection.getZosmfPort() +
+                ZosFilesConstants.RESOURCE + ZosFilesConstants.RES_USS_FILES +
+                EncodeUtils.encodeURIComponent(FileUtils.validatePath(targetPath));
+
+        final Map<String, Object> getFaclMap = new HashMap<>();
+        getFaclMap.put("request", "getfacl");
+        params.getType().ifPresent(type -> getFaclMap.put("type", type));
+        params.getUser().ifPresent(user -> getFaclMap.put("user", user));
+        if (params.getUseCommas())
+            getFaclMap.put("use-commas", params.getUseCommas());
+        if (params.getSuppressHeader())
+            getFaclMap.put("suppress-header", params.getSuppressHeader());
+        if (params.getSuppressBaseAcl())
+            getFaclMap.put("suppress-baseacl", params.getSuppressBaseAcl());
+
+        if (request == null) {
+            request = ZosmfRequestFactory.buildRequest(connection, ZosmfRequestType.PUT_JSON);
+        }
+        request.setUrl(url);
+        request.setBody(new JSONObject(getFaclMap).toString());
+
+        Response response = request.executeRequest();
+        return response;
+    }
 
 }
