6a5b9a0c0cc0f5b79f5035b94d5ec8ca455254da
Frank Giordano <giofr01@yahoo.com>
2025-02-26 22:01:47 -0500

    fix url link in javadoc

--- src/main/java/zowe/client/sdk/zosmfauth/methods/ZosmfPassword.java
+++ src/main/java/zowe/client/sdk/zosmfauth/methods/ZosmfPassword.java
@@ -27,7 +27,7 @@ import java.util.Map;
 /**
  * Provides z/OSMF change user password or passphrase service
  * <p>
- * <a> href="https://www.ibm.com/docs/en/zos/3.1.0?topic=services-change-user-password-passphrase">z/OSMF REST API </a>
+ * <a href="https://www.ibm.com/docs/en/zos/3.1.0?topic=services-change-user-password-passphrase">z/OSMF REST API </a>
  *
  * @author Esteban Sandoval
  * @author Frank Giordano
