7d2a3d2d099d77b23aabe427e580f21f5afa23ef
Frank Giordano <giofr01@yahoo.com>
2025-04-20 23:49:54 -0400

    CreateZfsParams tests

---
+++ src/test/java/zowe/client/sdk/zosfiles/CreateZfsParamsTest.java
@@ -0,0 +1,76 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package zowe.client.sdk.zosfiles;
+
+import org.junit.Test;
+import zowe.client.sdk.zosfiles.uss.input.CreateZfsParams;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Class containing unit tests for CreateZfsParams.
+ *
+ * @author Frank Giordano
+ * @version 3.0
+ */
+public class CreateZfsParamsTest {
+
+    @Test
+    public void tstCreateZfsParamsValueCheckSuccess() {
+        CreateZfsParams createZfsParams = new CreateZfsParams.Builder(10).build();
+        assertEquals(10, createZfsParams.getCylsPri().getAsInt());
+    }
+
+    @Test
+    public void tstCreateZfsParamsNullValueFailure() {
+        boolean isException = false;
+        try {
+            new CreateZfsParams.Builder(null).build();
+        } catch (Exception e) {
+            isException = true;
+        }
+        assertTrue(isException);
+    }
+
+    @Test
+    public void tstCreateZfsParamsNullValueMessageFailure() {
+        String errMsg = null;
+        try {
+            new CreateZfsParams.Builder(null).build();
+        } catch (Exception e) {
+            errMsg = e.getMessage();
+        }
+        assertEquals("cylsPri is null", errMsg);
+    }
+
+    @Test
+    public void tstCreateZfsParamsZeroValueMessageFailure() {
+        String errMsg = null;
+        try {
+            new CreateZfsParams.Builder(0).build();
+        } catch (Exception e) {
+            errMsg = e.getMessage();
+        }
+        assertEquals("specify cylsPri greater than 0", errMsg);
+    }
+
+    @Test
+    public void tstCreateZfsParamsNegativeValueMessageFailure() {
+        String errMsg = null;
+        try {
+            new CreateZfsParams.Builder(-1).build();
+        } catch (Exception e) {
+            errMsg = e.getMessage();
+        }
+        assertEquals("specify cylsPri greater than 0", errMsg);
+    }
+
+}
