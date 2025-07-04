461169570597a7e12946dd3e50d792d917d81144
Frank Giordano <giofr01@yahoo.com>
2025-07-03 22:49:42 -0400

    javadoc for AuthenticationType

--- src/main/java/zowe/client/sdk/core/AuthenticationType.java
+++ src/main/java/zowe/client/sdk/core/AuthenticationType.java
@@ -10,23 +10,27 @@
 package zowe.client.sdk.core;
 
 /**
+ * Class to represent an authentication type used for the http request.
  *
- *
- *
+ * @author Frank Giordano
+ * @version 4.0
  */
 public enum AuthenticationType {
 
     /**
-     *
+     * Authentication classic type. This represents the bearer header with the requirement
+     * for the username and password to be specified within ZosConnection object.
      */
     CLASSIC,
     /**
-     *
+     * Authentication cookie type. This case represents using a cookie value to use for
+     * authentication for the http request.
      */
     COOKIE,
     /**
-     *
+     * Authentication ssl type. This case represents using a certificate file for
+     * authentication for the http request.
      */
     SSL
-
+git st
 }
