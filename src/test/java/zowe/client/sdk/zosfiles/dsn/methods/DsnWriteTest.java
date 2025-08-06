1a5885d2f7ec585ed8ad15e2f51f064bd23fe48b
Frank Giordano <giofr01@yahoo.com>
2025-08-05 22:22:46 -0400

    header info

--- src/test/java/zowe/client/sdk/zosfiles/dsn/methods/DsnWriteTest.java
+++ src/test/java/zowe/client/sdk/zosfiles/dsn/methods/DsnWriteTest.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package zowe.client.sdk.zosfiles.dsn.methods;
 
 import kong.unirest.core.Cookie;
@@ -18,6 +27,12 @@ import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.withSettings;
 
+/**
+ * Class containing unit tests for DsnRename.
+ *
+ * @author Frank Giordano
+ * @version 4.0
+ */
 public class DsnWriteTest {
 
     private final ZosConnection connection = ZosConnectionFactory
