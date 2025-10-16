09244ac71ab0133cc2fe85b71e1bba0c01a99e9c
Frank Giordano <giofr01@yahoo.com>
2025-10-15 22:35:44 -0400

    clean up

--- src/test/java/zowe/client/sdk/utility/ConsoleUtilsTest.java
+++ src/test/java/zowe/client/sdk/utility/ConsoleUtilsTest.java
@@ -10,10 +10,12 @@
 package zowe.client.sdk.utility;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Class containing unit test for ConsoleUtils.
