130ead614b03359c27c399965a6800112ce292da
Ashish Kumar Dash <ashishdash2410@gmail.com>
2026-05-10 11:57:31 +0530

    Fix USS response package typo
    Signed-off-by: Ashish Kumar Dash <ashishdash2410@gmail.com>

--- src/main/java/zowe/client/sdk/zosfiles/uss/reaponse/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Response objects for z/OS Unix System Services (USS) files processing.
- *
- * @deprecated Use zowe.client.sdk.zosfiles.uss.response instead.
- */
-@Deprecated
-package zowe.client.sdk.zosfiles.uss.reaponse;
