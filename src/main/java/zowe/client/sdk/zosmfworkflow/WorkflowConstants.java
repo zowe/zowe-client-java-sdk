504a7645c516ac0665c99c684c3d531b9af38f10
AditheD <adithedas20@gmail.com>
2026-06-07 20:46:45 -0400

    fix: address review comments
    Signed-off-by: AditheD <adithedas20@gmail.com>

--- src/main/java/zowe/client/sdk/zosmfworkflow/WorkflowConstants.java
+++ src/main/java/zowe/client/sdk/zosmfworkflow/WorkflowConstants.java
@@ -1,10 +1,28 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package zowe.client.sdk.zosmfworkflow;
 
+/**
+ * Workflow API constants.
+ */
 public final class WorkflowConstants {
+
+    /**
+     * Prevent instantiation.
+     */
     private WorkflowConstants() {
         throw new IllegalStateException("Constants class");
     }
 
-    public static final String RESOURCE =
-            "/zosmf/workflow/rest/1.0/workflows";
+    /**
+     * Workflow REST resource path.
+     */
+    public static final String RESOURCE = "/zosmf/workflow/rest/1.0/workflows";
 }
--- src/main/java/zowe/client/sdk/zosmfworkflow/methods/WorkflowDelete.java
+++ src/main/java/zowe/client/sdk/zosmfworkflow/methods/WorkflowDelete.java
@@ -1,3 +1,12 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
 package zowe.client.sdk.zosmfworkflow.methods;
 
 import zowe.client.sdk.core.ZosConnection;
@@ -8,32 +17,60 @@ import zowe.client.sdk.rest.ZosmfRequestFactory;
 import zowe.client.sdk.rest.exception.ZosmfRequestException;
 import zowe.client.sdk.rest.type.ZosmfRequestType;
 import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.zosmfworkflow.WorkflowConstants;
 
-
+/**
+ * Delete a z/OSMF workflow instance.
+ */
 public class WorkflowDelete {
 
     private final ZosConnection connection;
+
     private ZosmfRequest request;
 
+    /**
+     * WorkflowDelete constructor.
+     *
+     * @param connection z/OS connection
+     */
     public WorkflowDelete(final ZosConnection connection) {
+
         ValidateUtils.checkNullParameter(connection, "connection");
+
         this.connection = connection;
     }
 
-    WorkflowDelete(final ZosConnection connection,
-                   final ZosmfRequest request) {
+    /**
+     * Alternative WorkflowDelete constructor for unit testing.
+     *
+     * @param connection z/OS connection
+     * @param request z/OSMF request instance
+     */
+    WorkflowDelete(final ZosConnection connection, final ZosmfRequest request) {
+
         ValidateUtils.checkNullParameter(connection, "connection");
+
         ValidateUtils.checkNullParameter(request, "request");
+
         this.connection = connection;
 
         if (!(request instanceof DeleteJsonZosmfRequest)) {
+
             throw new IllegalStateException("DELETE_JSON request type required");
         }
 
         this.request = request;
     }
 
+    /**
+     * Delete a workflow instance.
+     *
+     * @param workflowKey workflow key
+     * @return response object
+     * @throws ZosmfRequestException error executing request
+     */
     public Response delete(final String workflowKey)
+
             throws ZosmfRequestException {
 
         ValidateUtils.checkNullParameter(workflowKey, "workflowKey");
