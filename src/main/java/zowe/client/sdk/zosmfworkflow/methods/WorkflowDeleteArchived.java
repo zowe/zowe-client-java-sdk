7fe7dd1b8591924bd4e11b40e43354886c2e2972
Muhammad Imran <imrama@farmingdale.edu>
2026-06-09 13:10:34 -0400

    Add zosmfworkflow package with archived workflow DELETE and LIST APIs
    Closes #476
    Closes #478

---
+++ src/main/java/zowe/client/sdk/zosmfworkflow/methods/WorkflowDeleteArchived.java
@@ -0,0 +1,105 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package zowe.client.sdk.zosmfworkflow.methods;
+
+import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.DeleteJsonZosmfRequest;
+import zowe.client.sdk.rest.Response;
+import zowe.client.sdk.rest.ZosmfRequest;
+import zowe.client.sdk.rest.ZosmfRequestFactory;
+import zowe.client.sdk.rest.exception.ZosmfRequestException;
+import zowe.client.sdk.rest.type.ZosmfRequestType;
+import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.zosmfworkflow.WorkflowsConstants;
+
+/**
+ * Handles deleting an archived workflow on z/OS.
+ * <p>
+ * <a href="https://www.ibm.com/docs/en/zos/2.5.0?topic=services-zosmf-workflow">
+ *   z/OSMF REST API
+ * </a>
+ *
+ * @author Muhammad Imran
+ * @version 7.0
+ */
+public class WorkflowDeleteArchived {
+
+    private final ZosConnection connection;
+    private ZosmfRequest request;
+
+    /**
+     * WorkflowDeleteArchived constructor.
+     *
+     * @param connection for connection information, see ZosConnection object
+     * @author Muhammad Imran
+     */
+    public WorkflowDeleteArchived(final ZosConnection connection) {
+        ValidateUtils.checkNullParameter(connection, "connection");
+        this.connection = connection;
+    }
+
+    /**
+     * Alternative WorkflowDeleteArchived constructor with ZosmfRequest object. This is mainly used
+     * for internal code unit testing with Mockito, and it is not recommended to be used by the
+     * larger community.
+     * <p>
+     * This constructor is package-private
+     *
+     * @param connection for connection information, see ZosConnection object
+     * @param request    any compatible ZoweRequest Interface object
+     * @author Muhammad Imran
+     */
+    WorkflowDeleteArchived(final ZosConnection connection, final ZosmfRequest request) {
+        ValidateUtils.checkNullParameter(connection, "connection");
+        ValidateUtils.checkNullParameter(request, "request");
+        this.connection = connection;
+        if (!(request instanceof DeleteJsonZosmfRequest)) {
+            throw new IllegalStateException("DELETE_JSON request type required");
+        }
+        this.request = request;
+    }
+
+    /**
+     * Delete an archived workflow on z/OS.
+     *
+     * @param workflowKey workflow key of the archived workflow to delete
+     * @return http response object
+     * @throws ZosmfRequestException request error state
+     * @author Muhammad Imran
+     */
+    public Response delete(final String workflowKey) throws ZosmfRequestException {
+        return deleteCommon(workflowKey);
+    }
+
+    /**
+     * Delete an archived workflow on z/OS by workflow key.
+     *
+     * @param workflowKey workflow key of the archived workflow to delete
+     * @return http response object
+     * @throws ZosmfRequestException request error state
+     * @author Muhammad Imran
+     */
+    public Response deleteCommon(final String workflowKey) throws ZosmfRequestException {
+        ValidateUtils.checkIllegalParameter(workflowKey, WorkflowsConstants.WORKFLOW_KEY_ILLEGAL_MSG);
+
+        final String url = connection.getZosmfUrl() +
+                WorkflowsConstants.RESOURCE_ARCHIVED +
+                WorkflowsConstants.FILE_DELIM +
+                workflowKey;
+
+        if (request == null) {
+            request = ZosmfRequestFactory.buildRequest(connection, ZosmfRequestType.DELETE_JSON);
+        }
+        request.setUrl(url);
+
+        return request.executeRequest();
+    }
+
+}
---
+++ src/main/java/zowe/client/sdk/zosmfworkflow/methods/WorkflowListArchived.java
@@ -0,0 +1,114 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+package zowe.client.sdk.zosmfworkflow.methods;
+
+import org.json.simple.JSONArray;
+import zowe.client.sdk.core.ZosConnection;
+import zowe.client.sdk.rest.GetJsonZosmfRequest;
+import zowe.client.sdk.rest.ZosmfRequest;
+import zowe.client.sdk.rest.ZosmfRequestFactory;
+import zowe.client.sdk.rest.exception.ZosmfRequestException;
+import zowe.client.sdk.rest.type.ZosmfRequestType;
+import zowe.client.sdk.utility.JsonUtils;
+import zowe.client.sdk.utility.ValidateUtils;
+import zowe.client.sdk.zosmfworkflow.WorkflowsConstants;
+import zowe.client.sdk.zosmfworkflow.response.WorkflowKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Handles listing archived workflows on z/OS.
+ * <p>
+ * <a href="https://www.ibm.com/docs/en/zos/2.5.0?topic=services-zosmf-workflow">
+ *   z/OSMF REST API
+ * </a>
+ *
+ * @author Muhammad Imran
+ * @version 7.0
+ */
+public class WorkflowListArchived {
+
+    private final ZosConnection connection;
+    private ZosmfRequest request;
+
+    /**
+     * WorkflowListArchived constructor.
+     *
+     * @param connection for connection information, see ZosConnection object
+     * @author Muhammad Imran
+     */
+    public WorkflowListArchived(final ZosConnection connection) {
+        ValidateUtils.checkNullParameter(connection, "connection");
+        this.connection = connection;
+    }
+
+    /**
+     * Alternative WorkflowListArchived constructor with ZosmfRequest object. This is mainly used
+     * for internal code unit testing with Mockito, and it is not recommended to be used by the
+     * larger community.
+     * <p>
+     * This constructor is package-private
+     *
+     * @param connection for connection information, see ZosConnection object
+     * @param request    any compatible ZoweRequest Interface object
+     * @author Muhammad Imran
+     */
+    WorkflowListArchived(final ZosConnection connection, final ZosmfRequest request) {
+        ValidateUtils.checkNullParameter(connection, "connection");
+        ValidateUtils.checkNullParameter(request, "request");
+        this.connection = connection;
+        if (!(request instanceof GetJsonZosmfRequest)) {
+            throw new IllegalStateException("GET_JSON request type required");
+        }
+        this.request = request;
+    }
+
+    /**
+     * List all archived workflows on z/OS.
+     *
+     * @return list of WorkflowKey objects
+     * @throws ZosmfRequestException request error state
+     * @author Muhammad Imran
+     */
+    public List<WorkflowKey> listArchived() throws ZosmfRequestException {
+        return listArchivedCommon();
+    }
+
+    /**
+     * List all archived workflows on z/OS.
+     *
+     * @return list of WorkflowKey objects
+     * @throws ZosmfRequestException request error state
+     * @author Muhammad Imran
+     */
+    public List<WorkflowKey> listArchivedCommon() throws ZosmfRequestException {
+        final String url = connection.getZosmfUrl() + WorkflowsConstants.RESOURCE_ARCHIVED;
+
+        if (request == null) {
+            request = ZosmfRequestFactory.buildRequest(connection, ZosmfRequestType.GET_JSON);
+        }
+        request.setUrl(url);
+
+        final String responsePhrase = request.executeRequest()
+                .getResponsePhrase()
+                .orElseThrow(() -> new IllegalStateException("no list archived workflows response phrase"))
+                .toString();
+
+        final List<WorkflowKey> workflows = new ArrayList<>();
+        final JSONArray results = JsonUtils.parseArray(responsePhrase);
+        for (final Object obj : results) {
+            workflows.add(JsonUtils.parseResponse(String.valueOf(obj), WorkflowKey.class, "listArchivedCommon"));
+        }
+
+        return workflows;
+    }
+
+}
