5da3e3c15e4e7322e496cfcb80777384ee5359be
Ashish Kumar Dash <ashishdash2410@gmail.com>
2026-06-20 00:49:15 +0530

    Address review feedback
    Signed-off-by: Ashish Kumar Dash <ashishdash2410@gmail.com>

--- src/main/java/zowe/client/sdk/zosmfworkflow/WorkflowConstants.java
+++ src/main/java/zowe/client/sdk/zosmfworkflow/WorkflowConstants.java
@@ -60,8 +60,13 @@ public final class WorkflowConstants {
     public static final String ARCHIVED_WORKFLOWS_RESOURCE = BASE_RESOURCE + URL_PATH_DELIM + ARCHIVED_WORKFLOWS;
 
     /**
+     * Workflow definition resource segment.
+     */
+    public static final String WORKFLOW_DEFINITION = "workflowDefinition";
+
+    /**
      * z/OSMF workflow definition service path.
      */
-    public static final String WORKFLOW_DEFINITION_RESOURCE = BASE_RESOURCE + FILE_DELIM + "workflowDefinition";
+    public static final String WORKFLOW_DEFINITION_RESOURCE = BASE_RESOURCE + URL_PATH_DELIM + WORKFLOW_DEFINITION;
 
 }
