34679604bcc0f4345886139291c14f9662e4f200
frankgiordano <giofr01@yahoo.com>
2026-07-03 09:06:25 -0400

    clean up

--- src/test/java/zowe/client/sdk/zosvariables/methods/VariableCreateTest.java
+++ src/test/java/zowe/client/sdk/zosvariables/methods/VariableCreateTest.java
@@ -28,8 +28,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.*;
 
 /**
