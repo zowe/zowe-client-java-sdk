1275d58caa19b26a46eb529a10162eb4cabd0f24
Frank Giordano <giofr01@yahoo.com>
2026-07-20 23:42:36 -0400

    javadoc tweak

--- src/main/java/zowe/client/sdk/rest/package-info.java
+++ src/main/java/zowe/client/sdk/rest/package-info.java
@@ -4,7 +4,7 @@
  * UNIREST library provides the ability to retrieve JSON error document.
  * <p>
  * For example, the following http GET request will result in an HTTP 500 error:
- * https://xxxxxxx.broadcom.net:xxxx/zosmf/restfiles/ds?
+ * https://xxx.xxx.xxx:xxx/zosmf/restfiles/ds?
  * <p>
  * and the JSON error report document body response is:
  * {"rc":4,"reason":13,"category":1,"message":"query parm dslevel= or volser= must be specified"}
